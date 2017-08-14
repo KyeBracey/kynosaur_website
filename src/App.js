d2024f59a8dffdc798a36771ce61f80799d1fff3
Kye Bracey <kye.bracey@gmail.com>
2017-08-14 15:40:23 +0100

    Adds NavBar and Profile components, updates App.js to render both of these

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
+
 class App extends React.Component {
+  constructor() {
+    super()
+  }
+
   render() {
     return (
-      <div>Hello world!</div>
+      <div className='body'>
+        <NavBar />
+        <Profile />
+      </div>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className='NavBar'>
+        NavBar - buttons and such will go here.
+      </div>
+    )
+  }
+}
+
+export default NavBar;
