048cb0bbfeff1b07a5a8ed5c48a282af6648d69f
Kye Bracey <kye.bracey@gmail.com>
2017-08-17 17:11:04 +0100

    Adds styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import './stylesheets/NavBar.css';
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div className='NavBar'>
-        <button onClick={this.props.displayProfile}>
+      <div className='navBar'>
+        <button
+          className='profileButton'
+          onClick={this.props.displayProfile}
+        >
         Profile
         </button>
-        <button onClick={this.props.displayProjects}>
+        <button
+          className='projectsButton'
+          onClick={this.props.displayProjects}
+        >
           Projects
         </button>
-        <button onClick={this.props.displayContactDetails}>
+        <button
+          className='contactDetailsButton'
+          onClick={this.props.displayContactDetails}
+        >
           Contact
         </button>
       </div>
